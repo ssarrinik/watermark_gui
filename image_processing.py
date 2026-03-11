23a6ec7f295d8853c132021732ad429d5225798e
Σταμάτης Σαρρηνικολάου <176748942+ssarrinik@users.noreply.github.com>
2026-03-11 09:24:00 +0200

    Connect gui with image processing functionality

---
+++ image_processing.py
@@ -0,0 +1,32 @@
+from PIL import Image, ImageDraw, ImageFont
+from pathlib import Path
+
+class ImageProcessing:
+
+    def __init__(self):
+        self.img = None
+        self.image_width = None
+        self.image_height = None
+        self.x = None
+        self.y = None
+
+    def watermark(self, photo: str, text: str, path:str) -> None:
+        self.img = Image.open(f"{photo}").convert(mode="RGBA")
+        self.image_width, self.image_height = self.img.size
+        self.x = 0.0
+        self.y = 0.0
+
+        font = ImageFont.load_default(size=500)
+        text_layer = Image.new("RGBA", self.img.size, (255, 255, 255, 0))
+        draw_text_layer = ImageDraw.Draw(text_layer)
+
+        pos = draw_text_layer.textbbox((0, 0), text=text, font=font)
+        self.x = (self.image_width - (pos[2] - pos[0])) / 2
+        self.y = (self.image_height - (pos[3] - pos[1])) / 2
+
+        draw_text_layer.text((self.x, self.y), text, fill=(0, 0, 0, 150), font=font)
+
+        self.img = Image.alpha_composite(self.img, text_layer)
+        self.img.convert("RGB").save(path + photo.split('/')[-1])
+
+
--- watermark.py
+++ watermark.py
@@ -1,38 +1,87 @@
 import ttkbootstrap as ttk
 from tkinter import filedialog as fd
+from image_processing import ImageProcessing
 
-FONT = ("Helvetica", 12, "bold")
+FONT = ("Helvetica", 20)
 
 class WatermarkGui:
 
-    def __init__(self):
+    def __init__(self, image_proc: ImageProcessing):
+        self.image_processing = image_proc
+
+        self.to_watermark = []
+
         self.root = ttk.Window(themename="darkly")
         self.root.title('Watermark App')
         self.root.geometry('1000x800')
-
         self.frame = ttk.Frame(self.root)
+
         self.frame.place(relx=0.5, rely=0.5, anchor="center")
         self.selected_files = []
         self.path_labels = []
         self.notselected_label = None
         self.delete_buttons = []
+        self.delete_directory_btn = None
+        self.file_path = "Not directory selected."
 
         l1 = ttk.Label(self.frame, text="Watermark Text", font=FONT,  width=40)
         l1.grid(row=1, column=0, padx=40, pady=(20, 5), sticky='ew')
 
-        e1 = ttk.Entry(self.frame)
-        e1.grid(row=2, column=0, padx=40, pady=(5, 20), sticky='ew')
+        self.entry = ttk.Entry(self.frame)
+        self.entry.grid(row=2, column=0, padx=40, pady=(5, 20), sticky='ew')
+
+
+        self.button = ttk.Button(
+            self.frame,
+            text="Watermark",
+            state="disabled",
+            command= self.btn_watermark
+        )
+
+
+        self.button.grid(row=2, column=1, padx=40, pady=(5, 20), sticky='w')
+
+        choose_path_label = ttk.Label(self.frame, text="Output Directory", font=FONT, width=40)
+        choose_path_label.grid(row=3, column=0, padx=40, pady=(20, 5), sticky='ew')
+        choose_path_btn = ttk.Button(self.frame, text="Choose Output Directory", command=self.choose_output_path)
+        choose_path_btn.grid(row=4, column=0, padx=40, pady=(5, 3), sticky='ew', columnspan=2)
+
+        self.directory_path_label = ttk.Label(self.frame, text="Not directory selected", width=40)
+        self.directory_path_label.grid(row=5, column=0, padx=40, pady=(5,3), sticky='ew', columnspan=2)
 
         l2 = ttk.Label(self.frame, text="Select Target Images", font=FONT, width=40)
-        l2.grid(row=3, column=0, padx=40, pady=(20, 5), sticky='ew')
-
+        l2.grid(row=6, column=0, padx=40, pady=(20, 5), sticky='ew')
         b1 = ttk.Button(self.frame, text="Open file manager", command=self.open_files)
-        b1.grid(row=4, column=0, padx=40, pady=(5, 3), sticky='ew', columnspan=2)
+        b1.grid(row=7, column=0, padx=40, pady=(5, 3), sticky='ew', columnspan=2)
 
         self.notselected_label = ttk.Label(self.frame, text="No file selected", width=40)
-        self.notselected_label.grid(row=5, column=0, padx=40, pady=(5, 0), sticky='ew')
+        self.notselected_label.grid(row=8, column=0, padx=40, pady=(5, 0), sticky='ew')
 
-    def open_files(self):
+        self.root.after(200, self.check_button_responsiveness)
+
+    def choose_output_path(self) -> None:
+
+        self.file_path = fd.askdirectory()
+        self.directory_path_label.configure(text=self.file_path)
+
+        self.delete_directory_btn = ttk.Button(
+            self.frame,
+            text='X',
+            bootstyle='danger-link',
+            command= self.remove_directory)
+
+        self.delete_directory_btn.grid(row=5, column=1, padx=3, pady=(5,0), sticky='w')
+
+
+    def check_button_responsiveness(self) -> None:
+        if self.entry.get() and self.selected_files and self.file_path:
+            self.button.configure(state='')
+        else:
+            self.button.configure(state='disabled')
+
+        self.root.after(200, self.check_button_responsiveness)
+
+    def open_files(self) -> None:
         types = (("Image files", "*.jpg *.jpeg *.png"), ("all files", '*.*'))
 
         filepaths = fd.askopenfilenames(title="Open a file", filetypes=types)
@@ -64,10 +113,11 @@ class WatermarkGui:
             self.delete_buttons.append(btn)
 
         for i in range(len(self.path_labels)):
-            self.path_labels[i].grid(row=5+i, column=0 ,padx=40, pady=(5, 0), sticky='ew')
-            self.delete_buttons[i].grid(row=5+i, column=1, padx=3, pady=(5,0), sticky='w')
+            self.path_labels[i].grid(row=8+i, column=0 ,padx=40, pady=(5, 0), sticky='ew')
+            self.delete_buttons[i].grid(row=8+i, column=1, padx=3, pady=(5,0), sticky='w')
 
-    def remove_file(self, label, file):
+
+    def remove_file(self, label: ttk.Label, file: str) -> None:
         label.destroy()
         self.path_labels.remove(label)
         position = self.selected_files.index(file)
@@ -76,11 +126,37 @@ class WatermarkGui:
         self.delete_buttons.pop(position)
 
         for i in range(len(self.path_labels)):
-            self.path_labels[i].grid(row=5+i, column=0 ,padx=40, pady=(5, 0), sticky='ew')
-            self.delete_buttons[i].grid(row=5+i, column=1, padx=3, pady=(5,0), sticky='w')
+            self.path_labels[i].grid(row=8+i, column=0 ,padx=40, pady=(5, 0), sticky='ew')
+            self.delete_buttons[i].grid(row=8+i, column=1, padx=3, pady=(5,0), sticky='w')
 
         if not self.selected_files:
-            self.notselected_label.grid(row=5, column=0, padx=40, pady=(5, 0), sticky='ew')
+            self.notselected_label.grid(row=8, column=0, padx=40, pady=(5, 0), sticky='ew')
 
-watermark = WatermarkGui()
-watermark.root.mainloop()
+
+    def remove_directory(self) -> None:
+        self.file_path = "Not directory selected"
+        self.directory_path_label.configure(text=self.file_path)
+        self.delete_directory_btn.destroy()
+
+    def get_file_path(self) -> str | None:
+        return None if self.file_path == "Not directory selected" else self.file_path
+
+
+    def get_photos_to_watermark(self) -> list[str] | None:
+        return None if not self.selected_files else self.selected_files
+
+    def btn_watermark(self):
+        text = self.entry.get()
+        path = self.get_file_path()
+
+        for image in self.selected_files:
+            self.image_processing.watermark(image, text, path)
+
+
+        self.entry.delete(0, 50)
+
+
+
+if __name__ == "__main__":
+    watermark = WatermarkGui(ImageProcessing())
+    watermark.root.mainloop()
